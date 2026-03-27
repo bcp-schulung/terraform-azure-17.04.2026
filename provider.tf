ef2607638bb638a7319a1de64be855fe31530dc3
Ben Cöppicus <ben.coeppicus@it-scholar.com>
2026-03-27 10:41:01 +0100

    Add backend configuration for Azure storage in Terraform

--- provider.tf
+++ provider.tf
@@ -7,6 +7,14 @@ terraform {
       version = "~> 4.65.0"
     }
   }
+
+  backend "azurerm" {
+    use_azuread_auth = true
+    resource_group_name = "rg-tf-lab"
+    storage_account_name = "tfstateseminar"
+    container_name = "tfstate"
+    key = "terraform.tfstate"
+  }
 }
 
 provider "azurerm" {
